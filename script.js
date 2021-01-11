3a798efddef968bdddcb1138830f4d5c126f64d2
abineshwaran1997 <65437712+abineshwaran1997@users.noreply.github.com>
2021-01-11 15:06:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var request =new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload= function(){
+    var countrydata=JSON.parse(this.response);
+ for(var i in countrydata){
+    console.log(countrydata[i].name)
+ }
+   
+}
